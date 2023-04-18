60d23543d7e967db8537ad04287de5d1bcb0631b
legandcorepower <fgh50608@gmail.com>
2023-04-19 00:34:55 +0800

    mid porject

---
+++ utils/data/data.sql
@@ -0,0 +1,20 @@
+CREATE TABLE card_71 (
+    id int NOT NULL PRIMARY KEY,
+    title varchar(255),
+    descrip varchar(255),
+    dy varchar(255),
+    yr varchar(255),
+    mos varchar(255),
+    img varchar(255)
+)
+
+INSERT INTO card_71 (id,title,descrip,dy,yr,mos,img)
+
+VALUES
+(1,'Why Lead Generation is Key for Business Growth','A small river named Duden flows by their place and supplies it with the necessary regelialia.','21','2023','June','/images/photo-1.jpg'),
+(2,'Why Lead Generation is Key for Business Growth','A small river named Duden flows by their place and supplies it with the necessary regelialia.','21','2023','June','/images/photo-2.jpg'),
+(3,'Why Lead Generation is Key for Business Growth','A small river named Duden flows by their place and supplies it with the necessary regelialia.','21','2023','June','/images/photo-3.jpg'),
+(4,'Why Lead Generation is Key for Business Growth','A small river named Duden flows by their place and supplies it with the necessary regelialia.','21','2023','June','/images/photo-4.jpg'),
+(5,'Why Lead Generation is Key for Business Growth','A small river named Duden flows by their place and supplies it with the necessary regelialia.','21','2023','June','/images/photo-5.jpg'),
+(6,'Why Lead Generation is Key for Business Growth','A small river named Duden flows by their place and supplies it with the necessary regelialia.','21','2023','June','/images/photo-6.jpg'),
+
